e3b2f0f05dc7c335dd9e9a804bac8ada60bb70b4
NMMN1909 <danda_fluem@hotmail.com>
2019-01-11 13:28:52 +0100

    notice changes

--- Unity/Fake3DFaceTracking/Assets/Scripts/Tamagochi_AI/AI_Notice.cs
+++ Unity/Fake3DFaceTracking/Assets/Scripts/Tamagochi_AI/AI_Notice.cs
@@ -49,6 +49,7 @@ public class AI_Notice : MonoBehaviour {
         {
             controller.stopAllCoroutines = false;
             StartCoroutine(NoticeCycle());
+            stats.attention += Random.Range(15,25);
             stats.attention += Random.Range(10,20);
             canNotice = false;
         }
@@ -61,6 +62,7 @@ public class AI_Notice : MonoBehaviour {
         yield return new WaitForSeconds(stats.noticeDuration);
         if (card.isGiven)
         {
+            Debug.Log("Thoes sdapjd");
             stateMachine.State = AI_StateMachine.state.Respond;
             stats.attention += 50;
         }
